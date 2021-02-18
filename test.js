e3654a7f2339695d3d1ba2c8f8bfff4df5fe0ca8
moslixiansen <moslixiansen@163.com>
2021-02-18 18:05:03 +0800

    feat(算法笔记): 添加刷算法题笔记

--- test.js
+++ test.js
@@ -1,25 +1,40 @@
-async function async1() {
-    console.log("async1 start");
-    await  async2();
-    console.log("async1 end");
-}
+// async function async1() {
+//     console.log("async1 start");
+//     await  async2();
+//     console.log("async1 end");
+// }
 
-async  function async2() {
-    console.log( 'async2');
-}
+// async  function async2() {
+//     console.log( 'async2');
+// }
 
-console.log("script start");
+// console.log("script start");
 
-setTimeout(function () {
-    console.log("settimeout");
-},0);
+// setTimeout(function () {
+//     console.log("settimeout");
+// },0);
 
-async1();
+// async1();
 
-new Promise(function (resolve) {
-    console.log("promise1");
-    resolve();
-}).then(function () {
-    console.log("promise2");
-});
-console.log('script end'); 
+// new Promise(function (resolve) {
+//     console.log("promise1");
+//     resolve();
+// }).then(function () {
+//     console.log("promise2");
+// });
+// console.log('script end'); 
+
+
+// const a =  {
+//   value: 1,
+//   [Symbol.toPrimitive] () {
+//     return 2
+//   },
+//   valueOf () {
+//     return 3
+//   },
+//   toString() {
+//     return 4
+//   }
+// }
+// console.log(a)
