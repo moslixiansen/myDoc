59106d07ab65edab94bbcb15c6a8d194c1145509
lizhifang <moslixiansen@163.com>
2021-09-30 17:59:48 +0800

    更新复习进度

--- test.js
+++ test.js
@@ -38,3 +38,93 @@
 //   }
 // }
 // console.log(a)
+
+
+// function P (name, age) {
+//   this.name = name
+//   this.age = age
+//   return false
+// }
+// P.prototype.getName = function () {
+//   return this.name
+// }
+// var p = new P('leo', 10)
+// console.log(p)
+// console.log(p.getName())
+
+// function myNew() {
+//   const args = [ ...arguments ]
+//   const fn = args.shift()
+//   const obj = Object.create(fn.prototype)
+//   const f = Symbol('fn')
+//   obj[f] = fn
+//   const ret = obj[f](...args)
+//   delete obj[f]
+
+//   if(ret && typeof ret === 'object') {
+//     return ret
+//   } else {
+//     return obj
+//   }
+// }
+
+// var p1 = myNew(P, 'bob', 1)
+// console.log(p1) 
+// console.log(p1.getName())
+
+
+// if (!Object.create1) {
+//   Object.prototype.create1 = function(prototype) {
+//     const F = function() {}
+//     F.prototype = prototype
+//     console.log(F.prototype.constructor)
+//     return new F()
+//   }
+// }
+// function Super (name, age) {
+//   this.name = name
+//   this.age = age
+// }
+
+// Super.prototype.getName = function () {
+//   return this.name
+// }
+
+// function Sub (name, age, sex) {
+//   Super.call(this, name, age)
+//   this.sex = sex
+// }
+// Sub.prototype = Object.create1(Super.prototype)
+// Sub.prototype.getSex = function () {
+//   return this.sex
+// }
+// Sub.prototype.constructor = Sub
+
+
+// var s = new Super('O', 1)
+// console.log(s, s.getName())
+
+// var b = new Sub('L', 12, 'male')
+// console.log(b, b.getName(), b.getSex(), b.constructor)
+
+// if(!Function.prototype.apply) {
+//   Function.prototype.apply = function() {
+//     const fn = this
+//     const args = arguments
+//     const context = [  ]
+//   }
+// }
+
+
+// function mySymbol(str) {
+//   var obj = {}
+//   return function randomStr () {
+
+//     if (obj[str]) {
+//       randomStr(str)
+//     } else {
+
+//     }
+//   }
+// }
+
